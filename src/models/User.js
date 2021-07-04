2514c9341e0f170ab3aaff5fa7d5101d08faf1e9
Lapto-Vu <smallcounter@gmail.com>
2021-07-05 05:29:33 +0900

    AVTAR

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String },
-  avatarUrl: { type: String, default: "uploads/avatars/default_profile.jpeg", required: true},
+  avatarUrl: { type: String, default: "https://foxview.s3.ap-northeast-2.amazonaws.com/default_profile.jpeg", required: true},
   videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
   comments: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" }]
 });
