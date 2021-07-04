d7747ff3cdd1c5d7eefb53a7aedb651c2d9d7182
Lapto-Vu <smallcounter@gmail.com>
2021-07-05 05:17:14 +0900

    AWS

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 import multer from "multer";
-import multers3 from "multer-s3";
+import multerS3 from "multer-s3";
 import Video from "./models/Video";
 import User from "./models/User";
 import Comment from "./models/Comment";
